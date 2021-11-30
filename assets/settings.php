a680b7b42d6e49fe837d40581f53270837c9a363
terry zhang <zterry@qq.com>
2021-11-30 14:38:57 +0800

    init

---
+++ assets/settings.php
@@ -0,0 +1,87 @@
+<?php
+$config['devel.settings']['devel_dumper'] =  'var_dumper';
+$settings['config_sync_directory'] = '../config/sync';
+### Database connection
+if(getenv('MYSQL_HOST')){
+  $databases['default']['default'] = array(
+    'driver' => 'mysql',
+    'database' => getenv('MYSQL_DB_NAME'),
+    'username' => getenv('MYSQL_USERNAME'),
+    'password' => getenv('MYSQL_PASSWORD'),
+    'host' => getenv('MYSQL_HOST'),
+    'port' => getenv('MYSQL_PORT'),
+    'prefix' => '',
+  );
+}
+
+### ElasticSearch Config
+if (getenv('ES_URL')) {
+  $config['elasticsearch_connector.cluster.es01']['url'] = getenv('ES_URL');
+  $config['elasticsearch_connector.cluster.es01']['options']['username'] = getenv('ES_USERNAME');
+  $config['elasticsearch_connector.cluster.es01']['options']['password'] = getenv('ES_PASSWORD');
+}
+
+### Reverse proxy settings
+if (getenv('HTTP_X_FORWARDED_FOR')) {
+  $settings['reverse_proxy'] = TRUE;
+  $settings['reverse_proxy_addresses'] = [
+    getenv('REMOTE_ADDR'),
+    '127.0.0.1',
+  ];
+}
+
+### Trusted Host Patterns, see https://www.drupal.org/node/2410395 for more information.
+### If your site runs on multiple domains, you need to add these domains here
+$settings['trusted_host_patterns'] = array(
+  '^' . str_replace('.', '\.', getenv('VIRTUAL_HOST')) . '$',
+);
+
+### Temp directory
+if (getenv('TMP_PATH')) {
+  $config['system.file']['path']['temporary'] = getenv('TMP_PATH');
+}
+
+### Hash Salt
+if (getenv('HASH_SALT')) {
+  $settings['hash_salt'] = getenv('HASH_SALT');
+}
+
+// Services for all environments
+if (file_exists(__DIR__ . '/services.yml')) {
+  $settings['container_yamls'][] = __DIR__ . '/services.yml';
+}
+
+// Environment specific settings files.
+if(getenv('SITE_ENVIRONMENT')){
+  if (getenv('SITE_ENVIRONMENT') == 'prod') {
+    $config['system.logging']['error_level'] = 'hide';
+    $config['system.performance']['cache']['page']['max_age'] = 900;
+    $config['system.performance']['css']['preprocess'] = 1;
+    $config['system.performance']['js']['preprocess'] = 1;
+    $config['stage_file_proxy.settings']['origin'] = false;
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+  }
+  if (getenv('SITE_ENVIRONMENT') == 'dev') {
+    $config['system.logging']['error_level'] = 'verbose';
+    //$config['system.performance']['cache']['page']['max_age'] = 900;
+    $config['system.performance']['css']['preprocess'] = 0;
+    $config['system.performance']['js']['preprocess'] = 0;
+    //$config['stage_file_proxy.settings']['origin'] = false;
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+  }
+  // Environment specific services files.
+  if (file_exists(__DIR__ . '/' . getenv('SITE_ENVIRONMENT') . '.services.yml')) {
+    $settings['container_yamls'][] = __DIR__ . '/' . getenv('SITE_ENVIRONMENT') . '.services.yml';
+  }
+}
+
+// Last: this servers specific settings files.
+if (file_exists(__DIR__ . '/settings.local.php')) {
+  include __DIR__ . '/settings.local.php';
+}
+// Last: This server specific services file.
+if (file_exists(__DIR__ . '/services.local.yml')) {
+  $settings['container_yamls'][] = __DIR__ . '/services.local.yml';
+}
