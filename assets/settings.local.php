917e0c767bd8ebdbd3ff80f3db5f63c51ba2fc20
Terry <zterry@qq.com>
2022-01-05 15:24:17 +0800

    Update settings.local.php

--- assets/settings.local.php
+++ assets/settings.local.php
@@ -1,1 +1,12 @@
 <?php
+## $settings['config_sync_directory'] = '../config/sync';
+// $databases['default']['default'] = array (
+//   'database' => 'diatom_image_identification',
+//   'username' => '',
+//   'password' => '',
+//   'prefix' => '',
+//   'host' => '',
+//   'port' => '',
+//   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+//   'driver' => 'mysql',
+// );
