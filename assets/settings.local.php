a7ab03fbe5b092f544fe7a1ab8b0bb931093b173
Terry <zterry@qq.com>
2022-12-19 14:45:00 +0800

    add config sample

--- assets/settings.local.php
+++ assets/settings.local.php
@@ -12,3 +12,11 @@ $databases['default']['default'] = array (
   'driver' => 'mysql',
 );
 **/
+
+# $settings['maintenance_theme'] = 'claro';
+
+# $settings['config_sync_directory'] = '../config/sync';
+
+# $settings['file_public_path'] = 'sites/default/files';
+# $settings['file_private_path'] = '';
+# $settings['file_temp_path'] = '/tmp';
