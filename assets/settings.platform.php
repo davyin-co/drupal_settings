9fc0e3304d14db045de755c5b1306b3d7f347020
root <zterry@qq.com>
2025-04-25 21:45:48 +0800

    add redis support

--- assets/settings.platform.php
+++ assets/settings.platform.php
@@ -1,84 +1,130 @@
 <?php
+
 ### Database connection
-if(getenv('DB_HOST')){
-  $databases['default']['default'] = array(
-    'driver' => getenv('DB_TYPE') ? getenv('DB_TYPE') : 'mysql',
+if (!empty(getenv('DB_HOST'))) {
+  $databases['default']['default'] = [
+    'driver' => getenv('DB_TYPE') ?: 'mysql',
     'database' => getenv('DB_NAME'),
     'username' => getenv('DB_USERNAME'),
     'password' => getenv('DB_PASSWORD'),
     'host' => getenv('DB_HOST'),
     'port' => getenv('DB_PORT'),
-    //'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
     'prefix' => '',
     'collation' => 'utf8mb4_general_ci',
-  );
+  ];
+}
+
+### Redis config
+## ref: https://github.com/platformsh-templates/drupal9/blob/master/web/sites/default/settings.platformsh.php
+if (
+  !empty(getenv('REDIS_HOST')) &&
+  !empty(getenv('REDIS_PORT')) &&
+  !InstallerKernel::installationAttempted() &&
+  extension_loaded('redis') &&
+  class_exists('Drupal\redis\ClientFactory')
+) {
+  $settings['cache']['default'] = 'cache.backend.redis';
+  $settings['cache_prefix'] = getenv('APP_ID');
+
+  $settings['redis.connection'] = [
+    'host' => getenv('REDIS_HOST'),
+    'port' => getenv('REDIS_PORT'),
+    'password' => getenv('REDIS_PASSWORD'),
+    'persistent' => TRUE,
+  ];
+
+  $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+  $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
+
+  $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
+
+  $settings['bootstrap_container_definition'] = [
+    'parameters' => [],
+    'services' => [
+      'redis.factory' => [
+        'class' => 'Drupal\redis\ClientFactory',
+      ],
+      'cache.backend.redis' => [
+        'class' => 'Drupal\redis\Cache\CacheBackendFactory',
+        'arguments' => ['@redis.factory', '@cache_tags_provider.container', '@serialization.phpserialize'],
+      ],
+      'cache.container' => [
+        'class' => '\Drupal\redis\Cache\PhpRedis',
+        'factory' => ['@cache.backend.redis', 'get'],
+        'arguments' => ['container'],
+      ],
+      'cache_tags_provider.container' => [
+        'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
+        'arguments' => ['@redis.factory'],
+      ],
+      'serialization.phpserialize' => [
+        'class' => 'Drupal\Component\Serialization\PhpSerialize',
+      ],
+    ],
+  ];
 }
 
 ### ElasticSearch Config
-if (getenv('ES_URL')) {
-  $config['elasticsearch_connector.cluster.es01']['url'] = getenv('ES_URL');
-  $config['elasticsearch_connector.cluster.es01']['options']['username'] = getenv('ES_USERNAME');
-  $config['elasticsearch_connector.cluster.es01']['options']['password'] = getenv('ES_PASSWORD');
+if (!empty(getenv('ES_URL'))) {
+  $config['elasticsearch_connector.cluster.es01'] = [
+    'url' => getenv('ES_URL'),
+    'options' => [
+      'username' => getenv('ES_USERNAME'),
+      'password' => getenv('ES_PASSWORD'),
+    ],
+  ];
 }
 
 ### Reverse proxy settings
-## https://www.drupal.org/node/425990
-if (getenv('HTTP_X_FORWARDED_FOR')) {
+if (!empty(getenv('HTTP_X_FORWARDED_FOR'))) {
   $settings['reverse_proxy'] = TRUE;
-  $settings['reverse_proxy_trusted_headers'] = \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_HOST | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO | \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PORT |  \Symfony\Component\HttpFoundation\Request::HEADER_FORWARDED;
+  $settings['reverse_proxy_trusted_headers'] = 
+    \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR |
+    \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_HOST |
+    \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO |
+    \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PORT |
+    \Symfony\Component\HttpFoundation\Request::HEADER_FORWARDED;
+
   $settings['reverse_proxy_addresses'] = [
-    //getenv('HTTP_X_FORWARDED_FOR'),
     $_SERVER['REMOTE_ADDR'],
     '127.0.0.1',
   ];
 }
 
-### Trusted Host Patterns, see https://www.drupal.org/node/2410395 for more information.
-### If your site runs on multiple domains, you need to add these domains here
-### match example.com and example.com:8080
-if (strpos(getenv('HTTP_HOST'), ':') === FALSE) {
-  $host = getenv('HTTP_HOST');
-  $port = 80;
-}
-else {
-  list($host, $port) = explode(':', getenv('HTTP_HOST'));
-}
-$settings['trusted_host_patterns'] = array(
-  '^' . str_replace('.', '\.', $host) . '$',
+### Trusted Host Patterns
+$host = getenv('HTTP_HOST') ?: 'localhost';
+$host = strpos($host, ':') === FALSE ? $host : explode(':', $host)[0];
+$settings['trusted_host_patterns'] = [
+  '^' . preg_quote($host) . '$',
   '127.0.0.1',
   'localhost',
-);
+];
 
 ### Temp directory
-if (getenv('TMP_PATH')) {
+if (!empty(getenv('TMP_PATH'))) {
   $config['system.file']['path']['temporary'] = getenv('TMP_PATH');
 }
 
 ### Hash Salt
-if (getenv('HASH_SALT')) {
+if (!empty(getenv('HASH_SALT'))) {
   $settings['hash_salt'] = getenv('HASH_SALT');
 }
 
-// Environment specific settings files.
-if(getenv('SITE_ENVIRONMENT')){
-  if (getenv('SITE_ENVIRONMENT') == 'prod') {
-    $config['system.logging']['error_level'] = 'hide';
-    $config['system.performance']['cache']['page']['max_age'] = 900;
-    $config['system.performance']['css']['preprocess'] = 1;
-    $config['system.performance']['js']['preprocess'] = 1;
-    $config['stage_file_proxy.settings']['origin'] = false;
-    $config['dblog.settings']['row_limit'] = 100000;
-    ini_set('display_errors', 0);
-    ini_set('display_startup_errors', 0);
-  }
-  if (getenv('SITE_ENVIRONMENT') == 'dev') {
-    $config['system.logging']['error_level'] = 'verbose';
-    //$config['system.performance']['cache']['page']['max_age'] = 900;
-    $config['system.performance']['css']['preprocess'] = 0;
-    $config['system.performance']['js']['preprocess'] = 0;
-    //$config['stage_file_proxy.settings']['origin'] = false;
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-  }
+### Environment-specific settings
+$environment = getenv('SITE_ENVIRONMENT');
+if ($environment === 'prod') {
+  $config['system.logging']['error_level'] = 'hide';
+  $config['system.performance']['cache']['page']['max_age'] = 900;
+  $config['system.performance']['css']['preprocess'] = 1;
+  $config['system.performance']['js']['preprocess'] = 1;
+  $config['stage_file_proxy.settings']['origin'] = FALSE;
+  $config['dblog.settings']['row_limit'] = 100000;
+  ini_set('display_errors', 0);
+  ini_set('display_startup_errors', 0);
+} elseif ($environment === 'dev') {
+  $config['system.logging']['error_level'] = 'verbose';
+  $config['system.performance']['css']['preprocess'] = 0;
+  $config['system.performance']['js']['preprocess'] = 0;
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
 }
-
